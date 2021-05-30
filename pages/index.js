d00a79da187f9fb9301ae366f5a70d77085ecf42
miad2401 <miad2401@gmail.com>
2021-05-30 03:01:50 -0400

    Spruced up home page, added nav bar

--- pages/index.js
+++ pages/index.js
@@ -9,22 +9,49 @@ export default function Home() {
         <title>De'shon Mcneil</title>
       </Head>
 
+      <section className={styles.navigation}>
+        <div className={styles.nav1}>
+          <div className={styles.nav2}>
+            <div className={styles.nav3}>
+              <div className={styles.navName}>
+                <a aria-label="Go to De'shon.gg" href="/" title="Go to De'shon.gg" className={styles.navNameLink}>
+                <img src="/DMLogo.png" alt="DM Logo" className={styles.dmlogo} />
+                </a>
+              </div>
+              <div className={styles.navButtons}>
+                <a href="/about/" className={styles.navLink}>About Me</a>
+                <a href="/contact/" className={styles.navLink}>Contact</a>
+              </div>
+            </div>
+          </div>
+        </div>
+        <section className={styles.navDropDown} style={{height: 0, boxShadow: 0 }}>
+          <div style={{height: 120}}></div>
+          <section className={styles.navDropMenu}>
+            <div className={styles.navDropGrid}></div>
+          </section>
+        </section>
+      </section>
+
       <main className={styles.main}>
-        <div>
-          <h1 className={styles.title}>
-            Welcome!
-          </h1>
+        <div style={{paddingTop: 168, paddingBottom: 72}}>
+          <div>
+            <h1 className={styles.title}>
+              Welcome!
+            </h1>
+          </div>
+          <div className={styles.spacer}/>
+          <div>
+            <p className={styles.description}>
+              I'm De'shon McNeil, a wearer of many hats including:<br></br>
+              software developer, artist, and game developer.<br></br>
+              I strive to provide engaging experiences that inspire<br></br>
+              others to be creative and think outside the norm.
+            </p>
+          </div>
+          <div className={styles.spacer}/>
         </div>
-        <div className={styles.spacer}></div>
-        <div>
-          <p className={styles.description}>
-            I'm De'shon Mcneil, a wearer of many hats including:<br></br>
-            software developer, artist, and game developer.<br></br>
-            I strive to provide engaging experiences that inspire<br></br>
-            others to be creative and think outside the norm.
-          </p>
-        </div>
-        <div className={styles.spacer}></div>
+  
         <div className={styles.grid}>
           <a href="https://miad2401.itch.io/" className={styles.card}>
             <h3>Games</h3>
@@ -41,19 +68,7 @@ export default function Home() {
             <p>Discover my artwork.</p>
           </a>
         </div>
-        <div className={styles.spacer}></div>
       </main>
-
-      <footer className={styles.footer}>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <img src="/vercel.svg" alt="Vercel Logo" className={styles.logo} />
-        </a>
-      </footer>
     </div>
   )
 }
